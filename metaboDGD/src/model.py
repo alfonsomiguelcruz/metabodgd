78361269067b933e23853d9fb8ac40537b66d5e2
Alfonso Miguel Cruz <alfonso_cruz@dlsu.edu.ph>
2025-06-26 19:42:18 +0800

    cleaned code, added model.get_rep

--- metaboDGD/src/model.py
+++ metaboDGD/src/model.py
@@ -1,5 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
+from torch.optim import Adam
+
 from metaboDGD.src.decoder import Decoder
 from metaboDGD.src.latent  import GaussianMixtureModel, RepresentationLayer
 from metaboDGD.src.dataset import MetaboliteDataset
@@ -117,33 +119,69 @@ class MetaboDGD():
 
         ## TODO: Fix representation layer to not ask for n_sample and latent_dim
         rep_layer_init = RepresentationLayer(values=rep_init)
-        
-        return data_loader, rep_layer_init
 
+        rep_best = self.select_best_rep(data_loader=data_loader, rep_layer=rep_layer_init)
+
+        rep_layer_best = RepresentationLayer(values=rep_best)
+
+        optimizer = Adam(
+            params=rep_layer_best.parameters(),
+            lr=1e-3, # From lr_rep in training
+            weight_decay=0.0,
+            betas=[0.5, 0.7]
+        )
+
+        """
+        rep, dec_out, time = 
+            self._optimize_rep(
+                data_loader = data_loader, [DONE]
+                rep_layer = rep_layer_best,[DONE]
+                optimizer = optimizer,     [DONE]
+                n_comp = 1,                [1]
+                n_rep_per_comp = 1,        [EXEMPT]
+                epochs = epochs,           [100]
+                opt_num = 1)               [?]
+        """
+        n_metabolites = self.dec.nn[-1].out_features
+        dim_op = self.gmm.dim
+        n_comp_op = 1
+        n_samples_op = len(data_loader.dataset)
+        n_epochs = 25
+        for e in range(0, n_epochs):
+            optimizer.zero_grad()
+            for x, i in data_loader:
+                n_batch_size = len(i)
+                z_all = rep_layer_best()
+                # print(z_all.shape)
+
+                z_3d = z_all.view(n_samples_op, n_comp_op, dim_op)[i]
+
+                z = z_3d.view(n_batch_size * n_comp_op, dim_op)
+
+                dec_out = self.dec(z=z)
+
+                obs_counts = x.unsqueeze(1).expand(-1, n_comp_op ,-1)
+
+                pred_means = dec_out.view(n_batch_size, n_comp_op, n_metabolites)
+
+                recon_loss = self.dec.normal_layer.loss(obs_counts, pred_means)
+
+                recon_loss_sum = recon_loss.sum().clone()
+
+                gmm_loss = self.gmm(z)
+
+                gmm_loss_sum = gmm_loss.sum().clone()
+
+                total_loss = recon_loss_sum + gmm_loss_sum
+
+                total_loss.backward()
+            optimizer.step()
+            if e + 1 == n_epochs:
+                rep_final = rep_layer_best()
+                dec_out_final = self.dec(rep_final)
+                return rep_final, dec_out_final
         # 'n_samples' *  'n_comp' * 'n_rep_per_comp' , 'dim'
         # 'n_samples' , 'n_rep_per_comp' , 'n_comp' , 'dim'
         # 'n_samples_in_batch' * 'n_comp' * 'n_rep_per_comp' , 'dim'
-        
 
-        """
-        STEPS
-        1. DataLoader - DONE
-        2. Sample New Points from GMM Means - DONE
-        3. Initialize RepresentationLayer from [2] - DONE
-        4. Select the best representation from GMM Forward
-        5. Initialize RepresentationLayer from [4]
-        6. Initialize Optimizer (Adam)
-        7. Optimize Representation
-            for e in epoch:
-                optimizer.zero_grad()
-                for x, i in data_loader:
-                    z = rep_layer_from_[5]
-                    y  = dec(z)
-                    recon_loss
-                    gmm_loss
-                    total_loss
-                    total_loss.backward()
-                optimizer.step()
-        8. optimizer.zero_grad()
-        """
-        pass
+        optimizer.zero_grad()
